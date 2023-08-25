dbef8dcad45b9ea67ef99378974ee52a00a526ec
Augustine <waugustine@mtech.edu>
2023-08-25 13:17:39 -0600

    First lab instructions and solution

---
+++ 01-Hello_Python_Hello_World/GreetingsFromMe.py
@@ -0,0 +1,13 @@
+#########################################################################
+#
+# GreetingsFromMe.py
+#
+# Author: Will Augustine
+#
+# Description: Prints a greeting message including my name and
+#   something interesting about me to the console
+#
+#########################################################################
+
+print("Hello, my name is Will Augustine\n")
+print("I am the TA for this course.")
---
+++ 01-Hello_Python_Hello_World/HelloWorld.py
@@ -0,0 +1,11 @@
+#########################################################################
+#
+# HelloWorld.py
+#
+# Author: Will Augustine
+#
+# Description: Prints a typical 'hello world' message to the console
+#
+#########################################################################
+
+print("Hello World")
